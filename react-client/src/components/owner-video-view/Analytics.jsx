f771d9715cb6a1b1267d951ea007881183cbdf6e
Brian Fang <bfang212@gmail.com>
2018-04-12 12:00:02 -0400

    working on c3

--- react-client/src/components/owner-video-view/Analytics.jsx
+++ react-client/src/components/owner-video-view/Analytics.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import C3Chart from 'react-c3js';
+import 'c3/c3.css';
 
-class Analytics extends React.Component {
-  constructor(props) {
-    super(props);
+const LineChart = ({ data }) =>
+  <C3Chart data={{ json: data }} />;
+
+const BarChart = ({ data }) =>
+  <C3Chart data={{ json: data, type: 'bar' }} />;
+
+const chartData = {
+  line: {
+    data1: [30, 20, 50, 40, 60, 50],
+    data2: [200, 130, 90, 240, 130, 220],
+    data3: [300, 200, 160, 400, 250, 250]
+  },
+  bar: {
+    data1: [30, 200, 100, 400, 150, 250],
+    data2: [130, 100, 140, 200, 150, 50]
   }
+};
 
-  render() {
-    return (
-      <div id="analytics">
-        Analytic
-      </div>
-    )
-  }
-}
-
-export default Analytics;
+ReactDOM.render(
+  <div>
+    <LineChart data={chartData.line} />
+    <BarChart data={chartData.bar} />
+  </div>, document.getElementById('app')
+);
