ea5b8e3a1bc586a01866553837b877f03cbd6d1f
Xiang Wu <xiangwudev@gmail.com>
2018-06-04 21:21:03 -0400

    updated port

--- server/index.js
+++ server/index.js
@@ -138,6 +138,7 @@ app.delete('/timestamps', (req, res) => {
 
 //---------------------------------------------------------SERVER
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
   console.log('listening on port 3000!');
 });
